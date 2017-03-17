4e4c4f745cd5b763d76050470aac8d163f4ee056
Prudvi <prudvirazbanala@gmail.com>
2017-03-17 01:37:37 -0500

    Update WelcomeToJava.java

--- Java/Introduction/WelcomeToJava.java
+++ Java/Introduction/WelcomeToJava.java
@@ -1,60 +1,18 @@
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Gist
- @prudvirajbanala
- Sign out
- Watch 0
-  Star 0
-  Fork 0 prudvirajbanala/HackerRank
- Code  Issues 0  Pull requests 0  Projects 1  Wiki  Pulse  Graphs  Settings
-Branch: master Find file Copy pathHackerRank/Java_Loop_1.java
-8ca443c  an hour ago
-@prudvirajbanala prudvirajbanala Update Java_Loop_1.java
-1 contributor
-RawBlameHistory     
-50 lines (38 sloc)  858 Bytes
-/*
-Objective 
-In this challenge, we're going to use loops to help us do some simple math.
-Task 
-Given an integer, N, print its first 10 multiples. Each multiple Nxi(where 1<=i<=10) should be printed on a new line 
-in the form: N x i = result.
+/* Welcome to Java 
+Welcome to the world of Java! This is your first step to learn this wonderful language. Print "Hello World." and "Hello Java." in the console to complete this challenge. 
+(You should write a main function inside a public class called Solution; otherwise, your code will not compile.)
 Input Format
-A single integer, N.
-Constraints
-2<=N<=20
+There is no input for this problem.
 Output Format
-Print 10 lines of output; each line  (where 1<=i<=10) contains the  result of Nxi in the form: 
-N x i = result.
+Output the two sentences in separate lines. See sample output.
 Sample Input
-2
-Sample Output
-2 x 1 = 2
-2 x 2 = 4
-2 x 3 = 6
-2 x 4 = 8
-2 x 5 = 10
-2 x 6 = 12
-2 x 7 = 14
-2 x 8 = 16
-2 x 9 = 18
-2 x 10 = 20
+There is no input for this problem.
 */
-import java.util.Scanner;
 
-public class Java_Loop_1 {
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-	    int N = in.nextInt();
-	    for(int i=1; i<=10; i++){
-	        int r = N * i;
-	        System.out.println(N + " x " + i + " = " + r);
-	    }
-	}
-
+public class WelcomeToJava
+{
+    static public void main(String[] args){
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
 }
-
